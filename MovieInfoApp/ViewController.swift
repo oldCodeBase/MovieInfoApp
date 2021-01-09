4608bd7b60f6bbde4be1a117b65d45693faa580d
Ibraczar <44230748+Ibraczar@users.noreply.github.com>
2021-01-09 21:31:33 +0300

    Initial Commit

---
+++ MovieInfoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieInfoApp
+//
+//  Created by Ibragim Akaev on 09/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
